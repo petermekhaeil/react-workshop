1da81b31da81f062d9c6d6329245ed0f4c2d435b
Peter Mekhaeil <4616064+petermekhaeil@users.noreply.github.com>
2023-05-04 18:15:30 +0800

    add test

--- react-pokedex/src/App.test.jsx
+++ react-pokedex/src/App.test.jsx
@@ -1,9 +1,25 @@
-import { render } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import App from './App';
 
 describe('App', () => {
-  it('renders App component', () => {
+  it('renders App component', async () => {
     render(<App />);
+
+    // output the rendered HTML
+    screen.debug();
+
+    // test if elements exist
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
+    expect(screen.getByText('Add')).toBeInTheDocument();
+
+    // fire events
+    await userEvent.type(screen.getByRole('textbox'), 'pikachu');
+    await userEvent.click(screen.getByText('Add'));
+
+    // wait for update
+    waitFor(() => expect(screen.getByText(/pikachu/)).toBeInTheDocument());
   });
 });
